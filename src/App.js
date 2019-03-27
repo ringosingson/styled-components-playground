118e8a3b55c288e1284ea8ec8bd2fff5a0e8afcd
Ringo Singson <ringosingson@live.com>
2019-03-27 22:20:17 +0530

    props in styled-components

--- src/App.js
+++ src/App.js
@@ -4,7 +4,16 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const Heading = styled.h1`
-  font-size: 10rem;
+  font-size: 4rem;
+`;
+
+const Button = styled.button`
+  padding: 5px 10px;
+  border-radius: 4px;
+  color: white;
+  font-size: 2rem;
+  border: none;
+  background: ${({ type }) => (type === "cancel" ? "tomato" : "indigo")};
 `;
 
 class App extends Component {
@@ -13,9 +22,11 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <Button>Save</Button>
           <Heading>
             Edit <code>src/App.js</code> and save to reload.
           </Heading>
+          <Button type="cancel">Cancel</Button>
           <a
             className="App-link"
             href="https://reactjs.org"
