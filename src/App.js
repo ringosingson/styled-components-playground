50f9e17c21c33ed1893324fae2cb8868c5e79456
Ringo Singson <ringosingson@live.com>
2019-04-01 01:55:57 +0530

    css props(not so nice)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 import Header from "./layouts/Header";
 import GlobalStyle from "./Global";
 import { Button, Card } from "./elements";
+import styled from "styled-components/macro";
+
+const Fake = styled.main`
+  background: white;
+`;
 
 // const above = Object.keys(size).reduce((acc, label) => {
 //   acc[label] = (...args) => css`
@@ -37,7 +42,15 @@ class App extends Component {
       <div>
         <Header />
         <GlobalStyle />
-        <main>
+        <Fake>CSS FAKE </Fake>
+        <main
+          css={`
+            background: red;
+            h2 {
+              font-size: 100px;
+            }
+          `}
+        >
           <Card>
             <h2>Card Heading</h2>
             <Card.Button modifiers="cancel">Say Hello</Card.Button>
@@ -54,3 +67,10 @@ class App extends Component {
 }
 
 export default App;
+
+// const Fake = styled.main`
+//   background: red;
+//   h2 {
+//     font-size: 100px;
+//   }
+// `;
