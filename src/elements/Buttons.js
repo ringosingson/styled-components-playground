234bf5984b989fa5710dd5284b0a02a51bfe5a4b
Ringo Singson <ringosingson@live.com>
2019-03-31 23:08:59 +0530

    styling the button

--- src/elements/Buttons.js
+++ src/elements/Buttons.js
@@ -1,18 +1,34 @@
-import styled from 'styled-components';
-import { fixedTop } from '../utilities';
-
-const color = 'white';
+import styled from "styled-components";
+import { teal, elevation } from "../utilities";
 
 export const Button = styled.button`
   padding: 5px 20px;
   border-radius: 4px;
-  color: ${color};
+  color: white;
   font-size: 2rem;
   border: none;
-  background: indigo;
+  background: ${teal};
+  ${elevation[1]};
+  &:hover {
+    ${elevation[2]};
+  }
+  ${({ size }) => {
+    if (size === "small") {
+      return `
+        font-size: 1rem;
+        padding: 3px 10px;
+      `;
+    }
+  }}
+  ${({ type }) => {
+    if (type === "cancel") {
+      return `
+        background: tomato;
+      `;
+    }
+  }}
 `;
 
 export const CancelButton = styled(Button)`
   background: tomato;
-  ${fixedTop};
 `;
